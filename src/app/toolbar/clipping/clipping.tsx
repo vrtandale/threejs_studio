40349a472ed64d922a6085b7885a282814735e83
vrtandale <vrtandale@amcbridge.com>
2025-12-08 14:55:34 +0530

    feat: enhance ClippingTool; add PlaneHelper and optimize bounding box calculations

--- src/app/toolbar/clipping/clipping.tsx
+++ src/app/toolbar/clipping/clipping.tsx
@@ -7,12 +7,16 @@ const ClippingTool = () => {
     const { clippingPosition } = useClippingStore();
     const clippingPlane = React.useMemo(() => new THREE.Plane(new THREE.Vector3(1, 0, 0), clippingPosition), []) // proper plane
     const { scene, renderer } = useCanvasContext();
-
+    const helpers = React.useMemo(() => new THREE.Group(), []);
     const [bounds, setBounds] = React.useState<{
         min: number;
         max: number;
         range: number;
     } | null>(null);
+    
+    React.useEffect(() => {
+        helpers.add(new THREE.PlaneHelper(clippingPlane, Number(bounds?.max)*5, 0xff0000));
+    }, [bounds?.max,clippingPosition]);
     // Compute bounding box only once when scene is ready
     React.useEffect(() => {
         //@ts-ignore
@@ -20,18 +24,15 @@ const ClippingTool = () => {
         if (!obj) return
         let bbox = new THREE.Box3().setFromObject(obj!);
         let size = bbox.getSize(new THREE.Vector3());
+        scene.add(helpers);
         setBounds({
             min: bbox.min.x,
             max: bbox.max.x,
             range: size.x
         });
-    }, [scene.children, clippingPosition]);
-
-    React.useEffect(() => {
         if (!bounds) return console.log('Bounds not set yet');
         // Convert percent → world position
-        const worldPosition =
-            bounds.min + (bounds.range * (clippingPosition / 100));
+        const worldPosition = bounds.min + (bounds.range * (clippingPosition / 100));
         clippingPlane.constant = -worldPosition;
         clippingPlane.set(new THREE.Vector3(1, 0, 0), -worldPosition)
         renderer.localClippingEnabled = true;
